ff2f6bc76ae67dadcb38e8cb62d74be5a9f12bf8
VLSI1 ECo <vlsi1_4@paxos.inf.pucrs.br>
2025-09-09 21:49:30 -0300

    fix tb verifica todos valores possiveis

--- tb/tb_decoder.vhd
+++ tb/tb_decoder.vhd
@@ -9,15 +9,6 @@ architecture sim of tb_decoder is
     signal scancode_in : std_logic_vector(7 downto 0);
     signal ascii_cuv   : std_logic_vector(7 downto 0);
     signal ascii_golden: std_logic_vector(7 downto 0);
-
-    -- Lista apenas com os scancodes que apresentaram divergência
-    type scancode_array is array (natural range <>) of std_logic_vector(7 downto 0);
-    constant test_vectors : scancode_array := (
-        x"14", x"20", x"21", x"2B",
-        x"31", x"3D", x"43", x"49",
-        x"53", x"81", x"83", x"85"
-    );
-
 begin
 
     uut: entity work.decoder_scancode_ascii
@@ -34,10 +25,9 @@ begin
 
     stim_proc: process
     begin
-        for i in test_vectors'range loop
-            scancode_in <= test_vectors(i);
+        for i in 0 to 255 loop
+            scancode_in <= std_logic_vector(to_unsigned(i, 8));
             wait for 10 ns;
-
             assert (ascii_cuv = ascii_golden)
             report "ERRO -> Entrada (scancode): " & integer'image(to_integer(unsigned(scancode_in))) &
                 " | Esperado (Golden): " & integer'image(to_integer(unsigned(ascii_golden))) &
@@ -45,6 +35,6 @@ begin
             severity error;
         end loop;
 
-        wait; -- encerra simulação
+        wait; 
     end process;
 end sim;
